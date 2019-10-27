b70e6df81fc93c13afefef9e4eb7f7371a582ae4
Nathan Blanchard <nathanblanchard@Nathans-MacBook-Pro.local>
2019-10-27 13:33:32 -0400

    trying to seed data

--- migrations/001.do.create_hitchcocktober_movies.sql
+++ migrations/001.do.create_hitchcocktober_movies.sql
@@ -1,7 +1,7 @@
 CREATE TABLE hitchcocktober_movies (
   id INTEGER PRIMARY KEY NOT NULL,
   title TEXT NOT NULL,
-  overview TEXT,
+  overview TEXT NOT NULL,
   vote_count INTEGER,
   poster_path VARCHAR(1000),
   popularity INTEGER,
