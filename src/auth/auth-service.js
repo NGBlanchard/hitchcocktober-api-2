6a24a883ac7bb943b54e4c4bf2d4e19908ebcc37
Nathan Blanchard <nathanblanchard@Nathans-MacBook-Pro.local>
2019-10-29 10:38:50 -0400

    working on login

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -4,7 +4,7 @@ const config = require('../config')
 
 const AuthService = {
   getUserWithUserName(db, user_name) {
-    return db('hitchcocktober_users')
+    return db('curricula_users')
       .where({ user_name })
       .first()
   },
--- src/config.js
+++ src/config.js
@@ -2,4 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'hitchcock://postgres@localhost/hitchcocktober',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'hitchcock://postgres@localhost/hitchcocktober-test',
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+
 }
